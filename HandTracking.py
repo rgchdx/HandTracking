8bb16053fe8faf662be6641cedec43396e1313c0
Richie Dix <rgchdx@gmail.com>
2025-05-21 00:57:31 -0500

    Hand tracking basics implemented

--- HandTracking.py
+++ HandTracking.py
@@ -8,7 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -21,7 +22,20 @@ while True:
     
     if results.multi_hand_landmarks: 
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm) # print the id and landmark: up to 20
+                # does not give us in pixels but gives us in percentage
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h) # get the x and y coordinates
+                if id == 0: # wrist point
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
+    # Calculate FPS
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, "FPS: " + str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
     
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
